50574ee474b99140c7fe6ba1bb98173b3e17fda6
Axhar85 <azhararshad_ntu@yahoo.com>
2021-05-29 12:27:32 +0200

    adding burger menu react component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import BurgerMenu from './animation/BurgerMenu';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+     
+     <BurgerMenu/>
+    </div>
+  );
+}
+
+export default App;
